1ea14f622f33d4a0c9495dd19a1c61e01d4e5e4f
Rob Hurring <rob@ubrio.us>
2010-04-02 14:02:10 -0400

    should only have an NPC now

--- lib/wars/wars/fight.rb
+++ lib/wars/wars/fight.rb
@@ -1,18 +1,10 @@
 module Wars
   class Fight < ActiveRecord::Base
-    
     belongs_to :player
-    
     validates_presence_of :player
-    validates_presence_of :opponent
     
-    def opponent
-      if is_player?
-        Player.find(opponent_id)
-      else
-        Npc.find(opponent_id)
-      end
+    def npc
+      @npc ||= Npc.find(npc_id)
     end
-    
   end
 end
