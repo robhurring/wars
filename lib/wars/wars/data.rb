ac5fc881c4814c64faae6fe023a1f12f09740fc5
Rob Hurring <rob@ubrio.us>
2010-04-07 11:44:28 -0400

    tweaking the game data to add more equipment/products and renamed a few NPCs and products

--- lib/wars/wars/data.rb
+++ lib/wars/wars/data.rb
@@ -1,7 +1,7 @@
 module Wars
   module Data
     MaxLoan = 30_000
-    BookieName = 'Lumberg'
+    BookieName = 'Lumbergh'
     BookieTolerance = 30 # days
     Encounters = true
     EncounterRate = 5 # days
@@ -12,21 +12,25 @@ module Wars
       Equipment.new(:id => 1, :name => 'Messenger Bag', :limit => 1, :price => 10_000, :adds => :space, :amount => 30),
       Equipment.new(:id => 2, :name => 'Briefcase', :limit => 2, :price => 10_000, :adds => :space, :amount => 15),
       Equipment.new(:id => 3, :name => 'Backpack', :limit => 1, :price => 50_000, :adds => :space, :amount => 50),
-      Equipment.new(:id => 4, :name => 'Medical Kit', :limit => 999, :price => 750, :adds => :life, :amount => 10, :disposable => true),
+      Equipment.new(:id => 4, :name => 'Band-Aid', :limit => 999, :price => 750, :adds => :life, :amount => 10, :disposable => true),
       Equipment.new(:id => 5, :name => 'IBM Keyboard', :limit => 1, :price => 20_000, :adds => :strength, :amount => 25),
       Equipment.new(:id => 6, :name => 'Mousing Star', :limit => 1, :price => 100_000, :adds => :strength, :amount => 50),
       Equipment.new(:id => 7, :name => 'Peacoat', :limit => 1, :price => 25_000, :adds => :defense, :amount => 25),
+      Equipment.new(:id => 8, :name => 'Medical Kit', :limit => 999, :price => 6_000, :adds => :life, :amount => 100, :disposable => true),
       # Non-Shop Special Rewards
-      Equipment.new(:id => 100, :name => 'Orb of Life', :limit => 1, :price => 0, :adds => :life, :amount => 100),
-      Equipment.new(:id => 101, :name => 'Thor\'s Hammer', :limit => 1, :price => 0, :adds => :strength, :amount => 50),
-      Equipment.new(:id => 102, :name => 'Mustard Shirt', :limit => 1, :price => 0, :adds => :defense, :amount => 50)
+      Equipment.new(:id => 100, :name => 'Coffee Mug', :limit => 1, :price => 0, :adds => :life, :amount => 100),
+      Equipment.new(:id => 101, :name => 'Ron\'s Bat', :limit => 1, :price => 0, :adds => :strength, :amount => 50),
+      Equipment.new(:id => 102, :name => 'Mustard Colored Shirt', :limit => 1, :price => 0, :adds => :defense, :amount => 50),
+      Equipment.new(:id => 103, :name => 'Jim\'s Messenger Bag', :limit => 1, :price => 0, :adds => :space, :amount => 80)
     ]
         
     Products = [
-      Product.new(:id => 1, :name => 'Staples', :price_range => (10..70), :min_message => 'A truckload of staples has been hijacked! Cheap staples for everybody!', :max_message => 'All TPS reports must now be stapled! Staples are at an all-time high!'),
+      Product.new(:id => 1, :name => 'Paper', :price_range => (10..70), :min_message => 'The amazon rain forest has just succumb to loggers. Paper is extremely cheap!', :max_message => 'Where did all the trees go? Paper prices have gone up dramatically!'),
       Product.new(:id => 2, :name => 'Ties', :price_range => (200..500), :min_message => 'Employees are rebelling against Ties! Ties are extremely cheap!', :max_message => 'Management has mandated wearing of Ties. Ties are now extremely expensive!'),
-      Product.new(:id => 3, :name => 'Computers', :price_range => (900..1500), :min_message => 'You notice stacks of computers in the hallway! Sweet!', :max_message => 'Management is basing your bonus on computer usage time! Computer prices have sky rocketed!'),
-      Product.new(:id => 4, :name => 'Copier', :price_range => (2000..5200), :min_message => 'Somebody just threw a bunch of copiers out!', :max_message => 'Months end is approaching and people need to get their TPS repors copied!')
+      Product.new(:id => 3, :name => 'Dell Desktop', :price_range => (900..1500), :min_message => 'The new Windows OS is riddled with bugs! People are dumping their PCs into the dumpster!', :max_message => 'Microsoft is releasing a new OS! Computer prices have jumped!'),
+      Product.new(:id => 5, :name => 'Optimus Keyboard', :price_range => (2400..5000), :min_message => 'Art Lebedev Studios just threw out a bunch of failed Optimus prototypes!', :max_message => 'The Optimus keyboard is running out of stock! Prices are soaring!'),
+      Product.new(:id => 6, :name => 'MacBook Pro', :price_range => (3500..7800), :min_message => 'Apple\'s new hardware is riddled with DRM! People are selling MacBooks at an all time low!', :max_message => 'Apple is upgrading their hardware! Prices are skyrocketing!'),
+      Product.new(:id => 4, :name => 'Copier', :price_range => (10000..15000), :min_message => 'Somebody just threw a bunch of copiers out!', :max_message => 'Months end is approaching and people need to get their TPS repors copied!')
     ]
     
     Locations = [
@@ -52,40 +56,23 @@ module Wars
     ]
 
     HospitalInventory = [
-      Equipment.find(4)
+      Equipment.find(4),
+      Equipment.find(8)
     ]
             
     Stores = [
       Store.new(:location_id => 1, :name => 'a shady ATM', :sells => :bank),
-      Store.new(:location_id => 1, :name => 'Loans \'R Us', :sells => :loans),
-      Store.new(:location_id => 2, :name => 'Gift Shop', :sells => GiftShopInventory),
-      Store.new(:location_id => 2, :name => 'Sensual Healing Hospice', :sells => HospitalInventory),
-      Store.new(:location_id => 7, :name => 'Ajay\'s Survival Shop', :sells => SurvivalEquipment)
+      Store.new(:location_id => 1, :name => 'Lumbergh', :sells => :loans),
+      Store.new(:location_id => 2, :name => 'Company Gift Shop', :sells => GiftShopInventory),
+      Store.new(:location_id => 2, :name => 'Company Nurse', :sells => HospitalInventory),
+      Store.new(:location_id => 7, :name => 'Dwight\'s Survival Shop', :sells => SurvivalEquipment)
     ]
 
     Npcs = [
-      Npc.new(
-        :id => 1, 
-        :name => 'Hall Monitor', 
-        :strength => 20, 
-        :defense => 0, 
-        :life => 35, 
-        :rewards => Proc.new{ [:cash, (500..1_000).rand] },
-        :condition => Proc.new{ |p| p.strength < 50 }
-      ),
-      Npc.new(
-        :id => 2, 
-        :name => 'Annoying Co-Worker', 
-        :strength => 35, 
-        :defense => 0,
-        :life => 80, 
-        :rewards => Proc.new{ [Data.random_product, (1..5).rand] },
-        :condition => Proc.new{ |p| p.strength < 75 }
-      ),      
       # Bosses
       Npc.new(
         :id => 100,
-        :name => 'Ajay',
+        :name => 'Lumbergh',
         :strength => 65,
         :defense => 20,
         :life => 100,
@@ -94,7 +81,7 @@ module Wars
       ),
       Npc.new(
         :id => 101,
-        :name => 'Lumberg',
+        :name => 'Ron Livingston',
         :strength => 80,
         :defense => 30,
         :life => 200,
@@ -103,6 +90,15 @@ module Wars
       ),
       Npc.new(
         :id => 102,
+        :name => 'Jim Halpert',
+        :strength => 50,
+        :defense => 100,
+        :life => 250,
+        :rewards => Proc.new{ |p| Data.boss_reward(p, Equipment.find(103), (25_000..50_000)) },
+        :condition => Proc.new{ |p| p.strength >= 100 }
+      ),
+      Npc.new(
+        :id => 103,
         :name => 'Dwight Shrute',
         :strength => 80,
         :defense => 100,
@@ -112,14 +108,15 @@ module Wars
       )
     ]
     
-    # Some random NPCs
-    ['Michael Scott', 'Pam', 'Jim', 'Creed'].each do |npc_name|
+    # Normal random NPCs
+    ['Milton', 'Ajay', 'Bob #1', 'Bob #2', 'Michael', 'Pam', 'Creed', 'Andy', 'Phillis', 'Toby'].each do |npc_name|
       Npcs << Npc.new(
         :name => npc_name,
         :strength => (10..40).rand,
         :defense => (0..15).rand,
         :life => (85..125).rand,
-        :rewards => Proc.new{ rand(2) == 0 ? [Data.random_product, (1..5).rand] : [:cash, (500..2_000).rand] }
+        :rewards => Proc.new{ rand(2) == 0 ? [Data.random_product, (1..5).rand] : [:cash, (500..2_000).rand] },
+        :condition => Proc.new{ |p| p.strength < 100 }
       )
     end
 
